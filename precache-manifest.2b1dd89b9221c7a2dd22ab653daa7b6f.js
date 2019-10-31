4cc37ec117a61f731ebc9a59ff71a3e016fc9a62
josprins <jco.prins@gmail.com>
2019-10-31 14:27:51 +0100

    Updates

---
+++ precache-manifest.2b1dd89b9221c7a2dd22ab653daa7b6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c06526b99d0a8b9a1f685596cd6869",
+    "url": "/frontend-portfolio/index.html"
+  },
+  {
+    "revision": "f6562144cb786691182f",
+    "url": "/frontend-portfolio/static/css/main.e6df4dcc.chunk.css"
+  },
+  {
+    "revision": "cd4136997317144d8c6c",
+    "url": "/frontend-portfolio/static/js/2.674fc246.chunk.js"
+  },
+  {
+    "revision": "f6562144cb786691182f",
+    "url": "/frontend-portfolio/static/js/main.2e455423.chunk.js"
+  },
+  {
+    "revision": "2a7d59ab95109c116182",
+    "url": "/frontend-portfolio/static/js/runtime-main.0143b3fa.js"
+  },
+  {
+    "revision": "1d33bbf9831e94743610e4bb6e177817",
+    "url": "/frontend-portfolio/static/media/Results-Layout.1d33bbf9.png"
+  },
+  {
+    "revision": "72294641f0d69e764e67d963ff8f7a9b",
+    "url": "/frontend-portfolio/static/media/calculator.72294641.png"
+  },
+  {
+    "revision": "7aeaad70008321cc137db7d0b4efe18c",
+    "url": "/frontend-portfolio/static/media/gameshowapp.7aeaad70.png"
+  },
+  {
+    "revision": "9c87c3546913faea007171b1a2e124e9",
+    "url": "/frontend-portfolio/static/media/paradise.9c87c354.jpg"
+  },
+  {
+    "revision": "99ce0317f0ed5a48abb80f0ef6119b5e",
+    "url": "/frontend-portfolio/static/media/signupform.99ce0317.png"
+  },
+  {
+    "revision": "01d8d6f756e02688e7fb6ab17fe2b79f",
+    "url": "/frontend-portfolio/static/media/thisportfolio.01d8d6f7.png"
+  },
+  {
+    "revision": "5ddd83e3977ca90faed7449f375094ea",
+    "url": "/frontend-portfolio/static/media/todolist.5ddd83e3.png"
+  },
+  {
+    "revision": "0610ef6e9ce3c90d9747afcfb2c5f56b",
+    "url": "/frontend-portfolio/static/media/videoplayer.0610ef6e.png"
+  }
+]);
